b8e3849bbc3072c147986549df0fd38cb58af70e
Kate Hill <katelynjayne20@gmail.com>
2025-02-03 22:59:10 -0700

    auth tests done

--- src/routes/authRouter.test.js
+++ src/routes/authRouter.test.js
@@ -2,12 +2,13 @@ const request = require('supertest');
 const app = require('../service');
 
 const testUser = { name: 'pizza diner', email: 'reg@test.com', password: 'a' };
-let testUserAuthToken;
+let testUserAuthToken, testUserIdNum;
 
 beforeAll(async () => {
   testUser.email = Math.random().toString(36).substring(2, 12) + '@test.com';
   const registerRes = await request(app).post('/api/auth').send(testUser);
   testUserAuthToken = registerRes.body.token;
+  testUserIdNum = registerRes.body.user.id;
 });
 
 test('register', async () => {
@@ -31,6 +32,20 @@ test('login', async () => {
   // eslint-disable-next-line no-unused-vars
   const { password, ...user } = { ...testUser, roles: [{ role: 'diner' }] };
   expect(loginRes.body.user).toMatchObject(user);
+});
+
+test('update', async () => {
+  const newUser = {email: "newemail@test.com", password: testUser.password};
+  const updateRes = await request(app).put(`/api/auth/${testUserIdNum}`).set("Authorization", `Bearer ${testUserAuthToken}`).send(newUser);
+  expect(updateRes.status).toBe(200);
+  expect(updateRes.body.email).toBe(newUser.email);
+});
+
+test('update unauthorized', async () => {
+  const newUser = {email: "newemail@test.com", password: testUser.password};
+  const updateRes = await request(app).put(`/api/auth/${testUserIdNum+1}`).set("Authorization", `Bearer ${testUserAuthToken}`).send(newUser);
+  expect(updateRes.status).toBe(403);
+  expect(updateRes.body.message).toBe("unauthorized");
 });
 
 test('logout', async () => {
