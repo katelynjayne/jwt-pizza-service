b36706bd65c9039a639c9be229ac073beac9d929
Kate Hill <katelynjayne20@gmail.com>
2025-02-03 22:23:12 -0700

    logout test

--- src/routes/authRouter.test.js
+++ src/routes/authRouter.test.js
@@ -2,12 +2,12 @@ const request = require('supertest');
 const app = require('../service');
 
 const testUser = { name: 'pizza diner', email: 'reg@test.com', password: 'a' };
-// let testUserAuthToken;
+let testUserAuthToken;
 
 beforeAll(async () => {
   testUser.email = Math.random().toString(36).substring(2, 12) + '@test.com';
   const registerRes = await request(app).post('/api/auth').send(testUser);
-  registerRes.body.token;
+  testUserAuthToken = registerRes.body.token;
 });
 
 test('register', async () => {
@@ -28,7 +28,15 @@ test('login', async () => {
   const loginRes = await request(app).put('/api/auth').send(testUser);
   expect(loginRes.status).toBe(200);
   expect(loginRes.body.token).toMatch(/^[a-zA-Z0-9\-_]*\.[a-zA-Z0-9\-_]*\.[a-zA-Z0-9\-_]*$/);
+  // eslint-disable-next-line no-unused-vars
+  const { password, ...user } = { ...testUser, roles: [{ role: 'diner' }] };
+  expect(loginRes.body.user).toMatchObject(user);
+});
 
-  //const { password, ...user } = { ...testUser, roles: [{ role: 'diner' }] };
-  //expect(loginRes.body.user).toMatchObject(user);
+test('logout', async () => {
+  const loginRes = await request(app).put('/api/auth').send(testUser);
+  expect(loginRes).not.toBeNull();
+  const logoutRes = await request(app).delete('/api/auth').set("Authorization", `Bearer ${testUserAuthToken}`);
+  expect(logoutRes.status).toBe(200);
+  expect(logoutRes.body.message).toBe('logout successful');
 });
